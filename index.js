2b9ba185b54785c400398fea314e09fd3b685b34
Mazharul Islam <mie.mazharul@gmail.com>
2023-06-12 10:49:29 +0600

    feat: implement redux store with buyCake action and reducer

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('Hello World')
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const BUY_CAKE = 'BUY_CAKE';
+
+// region action creator
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+// endregion
+
+// region Reducer
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE:
+            return {
+                ...state, // Copy of the state object
+                numOfCakes: state.numOfCakes - 1
+            }
+
+        default:
+            return state
+    }
+}
+
+//endregion
+
+const store = createStore(reducer); // Holds application state
+console.log('Initial state', store.getState()); // Get initial state
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState())); // Listen to state changes
+store.dispatch(buyCake()); // Dispatch action
+store.dispatch(buyCake()); // Dispatch action
+store.dispatch(buyCake()); // Dispatch action
+
+unsubscribe(); // Unsubscribe from state changes
